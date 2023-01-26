99dd8282f22d3abee95ce6e44ce978ce8dd7e26e
jy.choi <cjy2103@naver.com>
2023-01-26 14:00:41 +0900

    프로그래머스 k번째 수 포스팅 완

--- JAVA/New/Sort/k_number/k_number/src/Solution.java
+++ JAVA/New/Sort/k_number/k_number/src/Solution.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 public class Solution {
 
     public int[] solution(int[] array, int[][] commands){
-        int[] answer = {};
+        int[] answer = new int[commands.length];
+
+        for(int pos= 0;pos<commands.length;pos++){
+            int i = commands[pos][0];
+            int j = commands[pos][1];
+            int k = commands[pos][2];
+            int[] copyArr = new int[j-i+1];
+            System.arraycopy(array, i-1, copyArr, 0 , copyArr.length);
+            Arrays.sort(copyArr);
+            answer[pos] = copyArr[k-1];
+        }
+
         return answer;
     }
 }
