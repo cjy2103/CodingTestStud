c9645077dc87af8d2ff573419872c2a40ec6986d
jy.choi <cjy2103@naver.com>
2023-02-08 14:53:08 +0900

    같은 숫자는 싫어 포스팅 완

--- JAVA/New/Stack,Queue/HateSameNumber/HateSameNumber/src/Solution.java
+++ JAVA/New/Stack,Queue/HateSameNumber/HateSameNumber/src/Solution.java
@@ -12,11 +12,9 @@ public class Solution {
             }
         }
 
-        int size = stack.size();
+        int[] answer = new int[stack.size()];
 
-        int[] answer = new int[size];
-
-        for(int i=size-1;i>=0;i--){
+        for(int i= answer.length-1;i>=0;i--){
             answer[i] = stack.pop();
         }
 
