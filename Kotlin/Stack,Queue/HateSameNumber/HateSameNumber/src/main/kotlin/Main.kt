3cb0a04afc5a6c792bf2998e4ef963a9db2e1538
jy.choi <cjy2103@naver.com>
2023-02-10 10:49:35 +0900

    코트린 같은 숫자는 싫어! Queue로 구현 완

--- Kotlin/Stack,Queue/HateSameNumber/HateSameNumber/src/main/kotlin/Main.kt
+++ Kotlin/Stack,Queue/HateSameNumber/HateSameNumber/src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main(args: Array<String>) {
 
+    val array = intArrayOf(1, 1, 3, 3, 0, 1, 1)
+//    val array = intArrayOf(4, 4, 4, 3, 3)
+//    val array = intArrayOf(0, 1, 2, 3, 4, 5)
+
+    val solution = Solution()
+    val answer = solution.solution(array)
+    println(answer.contentToString())
 }
--- Kotlin/Stack,Queue/HateSameNumber/HateSameNumber/src/main/kotlin/Solution.kt
+++ Kotlin/Stack,Queue/HateSameNumber/HateSameNumber/src/main/kotlin/Solution.kt
@@ -1,9 +1,17 @@
+import java.util.*
+
 class Solution {
     fun solution(array: IntArray): IntArray {
+        val queue = LinkedList<Int>()
+        var temp = -1
 
+        array.forEach { number ->
+            if (temp != number) {
+                queue.add(number)
+                temp = number
+            }
+        }
 
-        val answer = IntArray(5)
-
-        return answer
+        return queue.toIntArray()
     }
 }
