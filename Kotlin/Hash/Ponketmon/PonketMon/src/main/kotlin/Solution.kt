d8c4c025a4824f809006d7b5df701053afdd5601
jy.choi <cjy2103@naver.com>
2023-02-20 17:58:43 +0900

    한줄로 변경

--- Kotlin/Hash/Ponketmon/PonketMon/src/main/kotlin/Solution.kt
+++ Kotlin/Hash/Ponketmon/PonketMon/src/main/kotlin/Solution.kt
@@ -1,7 +1,6 @@
 class Solution {
 
     fun solution(nums : IntArray) : Int{
-        val limit = nums.size/2
-        return nums.toSet().size.coerceAtMost(limit)
+        return nums.toSet().size.coerceAtMost(nums.size/2)
     }
 }
