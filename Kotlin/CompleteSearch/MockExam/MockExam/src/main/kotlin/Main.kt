ca0b6387466f140a345e82c48257b0bb8a3859cd
jy.choi <cjy2103@naver.com>
2023-04-22 10:39:17 +0900

    프로그래머스 코틀린 모의고사 포스팅 완

--- Kotlin/CompleteSearch/MockExam/MockExam/src/main/kotlin/Main.kt
+++ Kotlin/CompleteSearch/MockExam/MockExam/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+//    val answers = intArrayOf(1, 2, 3, 4, 5)
+    val answers = intArrayOf(1, 3, 2, 4, 2)
+    val solution = Solution()
+    val answer = solution.solution(answers)
+    println(answer.contentToString())
 }
