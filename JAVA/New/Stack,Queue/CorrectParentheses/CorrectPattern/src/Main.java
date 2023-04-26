0b5d328b61b5453d4caa9e9c8a2aaff971d2681e
jy.choi <cjy2103@naver.com>
2023-04-27 03:06:19 +0900

    올바른 괄호 준비

---
+++ JAVA/New/Stack,Queue/CorrectParentheses/CorrectPattern/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "()()";
+        Solution solution = new Solution();
+        boolean answer = solution.solution(s);
+        System.out.println(s);
+    }
+}
---
+++ JAVA/New/Stack,Queue/CorrectParentheses/CorrectPattern/src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
