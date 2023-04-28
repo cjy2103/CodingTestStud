edf462ecc026af45e130dd2ca84f076ca2bab1ff
jy.choi <cjy2103@naver.com>
2023-04-28 17:03:39 +0900

    저장

--- JAVA/New/CompleteSearch/FindDecimals/FindDecimal/src/Solution.java
+++ JAVA/New/CompleteSearch/FindDecimals/FindDecimal/src/Solution.java
@@ -1,6 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int solution(String numbers) {
-        int answer = 0;
+        int          answer = 0;
+
+        // 만들수 있는 모든 숫자
+        Set<Integer> set    = new HashSet<>();
+        p
+
         return answer;
     }
 }
