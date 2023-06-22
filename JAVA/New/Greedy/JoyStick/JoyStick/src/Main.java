9a259be4df50bb6a58e97a4b4a01aa02e755887f
jy.choi <cjy2103@naver.com>
2023-06-22 21:04:29 +0900

    조이스틱 포스팅 완

--- JAVA/New/Greedy/JoyStick/JoyStick/src/Main.java
+++ JAVA/New/Greedy/JoyStick/JoyStick/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "JAN";
+        String name = "JEN";
         Solution solution = new Solution();
         int result = solution.solution(name);
         System.out.println(result);
