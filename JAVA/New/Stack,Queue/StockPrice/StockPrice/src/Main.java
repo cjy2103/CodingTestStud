d6e886d5f812933593dd291285a01a2d0291706c
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-07-06 11:11:15 +0900

    주식가격 포스팅 완

--- JAVA/New/Stack,Queue/StockPrice/StockPrice/src/Main.java
+++ JAVA/New/Stack,Queue/StockPrice/StockPrice/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        int[] prices = {1,2,3,4,5};
+//        int[] prices = {3,2,3,4,5};
+        int[] prices = {1,2,3,2,3};
+        Solution solution = new Solution();
+        int [] result = solution.solution(prices);
+        System.out.println(Arrays.toString(result));
     }
 }
