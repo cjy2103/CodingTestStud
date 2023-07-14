899d5b45fbf597968c5da346b5ff8c1999eaf14e
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-07-14 11:34:28 +0900

    추억점수 포스팅 완

--- JAVA/New/etc/MemoryScore/MemoryScore/src/Main.java
+++ JAVA/New/etc/MemoryScore/MemoryScore/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] name = {"may","kein","kain","radi"};
+        int[] yearning = {5,10,1,3};
+        String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+
+        Solution solution = new Solution();
+        int[] result = solution.solution(name,yearning,photo);
+        System.out.println(Arrays.toString(result));
     }
 }
--- JAVA/New/etc/MemoryScore/MemoryScore/src/Solution.java
+++ JAVA/New/etc/MemoryScore/MemoryScore/src/Solution.java
@@ -1,2 +1,20 @@
-public class Solution {
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> people = new HashMap<>();
+        int[] answer = new int[photo.length];
+
+        for(int i=0;i<name.length;i++){
+            people.put(name[i],yearning[i]);
+        }
+
+        for(int i=0;i<photo.length;i++){
+            int score = 0;
+            for(String person : photo[i]){
+                score += people.getOrDefault(person,0);
+            }
+            answer[i] = score;
+        }
+        return answer;
+    }
 }
