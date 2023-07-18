0a8e85ad55f6bc8106c7a523a732892667c83d29
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-07-18 11:14:49 +0900

    둘만의 암호 포스팅 완

--- JAVA/New/etc/Cipher/cipher/src/Main.java
+++ JAVA/New/etc/Cipher/cipher/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "aukks";
+        String skip = "wbqd";
+        int index = 5;
+        Solution solution = new Solution();
+        String result = solution.solution(s,skip,index);
+        System.out.println(result);
+
+
     }
 }
--- JAVA/New/etc/Cipher/cipher/src/Solution.java
+++ JAVA/New/etc/Cipher/cipher/src/Solution.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        return answer;
+        List<Character> alpha = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
+
+        for(char c = 'a'; c <= 'z'; c++){
+            if (!skip.contains(String.valueOf(c))){
+                alpha.add(c);
+            }
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int pos = alpha.indexOf(c) + index;
+            result.append(alpha.get(pos % alpha.size()));
+        }
+        return result.toString();
     }
 }
