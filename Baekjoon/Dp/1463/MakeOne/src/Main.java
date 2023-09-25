a1e03bc12ef8e8b1b8ed7ff312a88ed97a9c9039
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-09-25 17:25:20 +0900

    코드 검증중

--- Baekjoon/Dp/1463/MakeOne/src/Main.java
+++ Baekjoon/Dp/1463/MakeOne/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Solution solution = new Solution();
+
+        int result = solution.solution(N);
+        System.out.println(result);
     }
 }
--- Baekjoon/Dp/1463/MakeOne/src/Solution.java
+++ Baekjoon/Dp/1463/MakeOne/src/Solution.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution {
+    public int solution(int N){
+        int [] dp = new int[N + 1];
+        dp[1] = 0; // 1은 이미 1이기 때문에 연산 X
+
+        for(int i = 2; i<=N; i++){
+            dp[i] = dp[i-1] + 1;
+
+            if(i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+
+        }
+
+        return dp[N];
+    }
 }
