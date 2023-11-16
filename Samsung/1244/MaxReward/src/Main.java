0d696e6f387e4472112a82e8a41f36a52d78bef3
jy.choi <cjy2103@naver.com>
2023-11-16 13:43:47 +0900

    1244 문제 해결중...

--- Samsung/1244/MaxReward/src/Main.java
+++ Samsung/1244/MaxReward/src/Main.java
@@ -1,17 +1,111 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+    static int maxPrize;
+
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            maxPrize = 0; // 각 테스트 케이스마다 최대 상금 초기화
+
+            // 숫자판의 정보와 교환 횟수 입력
+            String number = sc.next();
+            int K = sc.nextInt();
+
+            // DFS 수행
+            dfs(number.toCharArray(), K, 0);
+
+            // 출력
+            System.out.println("#" + test_case + " " + maxPrize);
+
+//            String input = sc.next();
+//
+//            String[] target = input.split("");
+//
+//            int chance = sc.nextInt();
+//
+//            int answer = dfs(target, chance, 0, 0);
+//
+//            System.out.println("#" + test_case + " " + answer);
+
+        }
+    }
+
+//    static int dfs(String[] target, int chance, int k, int count) {
+//        String temp;
+//        String targetnum = "";
+//
+//        if (chance == count) {
+//            for (String tmp : target) {
+//                targetnum += tmp;
+//            }
+//            return Integer.parseInt(targetnum);
+//        }
+//
+//        int maxResult = 0;
+//
+//        for (int i = k; i < target.length; i++) {
+//            for (int j = i + 1; j < target.length; j++) {
+//
+//                if (Integer.parseInt(target[i]) <= Integer.parseInt(target[j])) {
+//                    temp = target[i];
+//                    target[i] = target[j];
+//                    target[j] = temp; // swap
+//                    int result = dfs(target, chance, i, count + 1);
+//                    maxResult = Math.max(maxResult, result);
+//                    temp = target[i];
+//                    target[i] = target[j];
+//                    target[j] = temp; // swap
+//                }
+//            }
+//        }
+//        return maxResult;
+//    }
+
+    private static void dfs(char[] digits, int k, int depth) {
+        if (k == 0 || depth == digits.length) {
+            int number = Integer.parseInt(new String(digits));
+            maxPrize = Math.max(maxPrize, number);
+            return;
+        }
+
+        for (int i = depth; i < digits.length; i++) {
+            for (int j = i + 1; j < digits.length; j++) {
+                // 현재 위치와 다음 위치의 값을 교환
+                char temp = digits[i];
+                digits[i] = digits[j];
+                digits[j] = temp;
+
+                // DFS 호출
+                dfs(digits, k - 1, depth + 1);
+
+                // 다시 원래대로 복구
+                temp = digits[i];
+                digits[i] = digits[j];
+                digits[j] = temp;
+            }
+        }
+
+        if (k > 0 && digits.length >= 2) {
+            char temp = digits[digits.length - 1];
+            digits[digits.length - 1] = digits[digits.length - 2];
+            digits[digits.length - 2] = temp;
+
+            // DFS 호출
+            dfs(digits, k - 1, depth + 1);
+
+            // 다시 원래대로 복구
+            temp = digits[digits.length - 1];
+            digits[digits.length - 1] = digits[digits.length - 2];
+            digits[digits.length - 2] = temp;
         }
     }
 }
