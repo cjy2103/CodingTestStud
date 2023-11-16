29eb425416b04793abff4cb1372f91a9bd27f207
jy.choi <cjy2103@naver.com>
2023-11-16 21:30:19 +0900

    1926

--- Samsung/1926/game/src/Main.java
+++ Samsung/1926/game/src/Main.java
@@ -1,17 +1,45 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for(int i = 1; i <= T; i++)
+        {
+            boolean contains369 = false;
+            int num = i;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            while (num != 0) {
+                int digit = num % 10;
+                if (digit == 3 || digit == 6 || digit == 9) {
+                    contains369 = true;
+                    break;
+                }
+                num /= 10;
+            }
+
+            if (contains369) {
+                int count369 = 0;
+                num = i;
+
+                while (num != 0) {
+                    int digit = num % 10;
+                    if (digit == 3 || digit == 6 || digit == 9) {
+                        count369++;
+                    }
+                    num /= 10;
+                }
+
+                for (int j = 0; j < count369; j++) {
+                    System.out.print("-");
+                }
+            } else {
+                System.out.print(i);
+            }
+
+            System.out.print(" ");
         }
     }
 }
