b83a403cc0768c16f3ab8f9881c62c473a5818d1
jy.choi <cjy2103@naver.com>
2023-11-18 21:59:01 +0900

    SWEA 1986 완

--- Samsung/1986/Number/src/Main.java
+++ Samsung/1986/Number/src/Main.java
@@ -1,17 +1,32 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            int N = sc.nextInt();
+
+            // 누적된 값
+            int sum = 0;
+
+            // 1부터 N까지의 숫자를 순회하면서 홀수면 더하고 짝수면 뺀다.
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 == 1) {
+                    sum += i;
+                } else {
+                    sum -= i;
+                }
+            }
+
+            // 누적된 값을 출력한다.
+            System.out.println("#" + test_case + " " + sum);
+
         }
+
     }
 }
