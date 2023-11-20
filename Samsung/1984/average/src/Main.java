cd91a3ea1f4e8527d62efa7f68b21004d43ffc73
jy.choi <cjy2103@naver.com>
2023-11-20 23:02:35 +0900

    SWEA 1984 포스팅 완

--- Samsung/1984/average/src/Main.java
+++ Samsung/1984/average/src/Main.java
@@ -1,17 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int[] numbers = new int[10];
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            // 10개의 수 입력
+            for (int i = 0; i < 10; i++) {
+                numbers[i] = sc.nextInt();
+            }
+
+            // 배열 정렬
+            Arrays.sort(numbers);
+
+            // 최대값과 최소값 제외한 합 계산
+            int sum = 0;
+            for (int i = 1; i < 9; i++) {
+                sum += numbers[i];
+            }
+
+            // 평균값 계산 및 반올림
+            int average = Math.round((float) sum / 8);
+
+            System.out.println("#" + test_case + " " + average);
         }
     }
 }
